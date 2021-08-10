588d65df3ffabf60b0959bf496953904b42d8b67
Mikołaj Kostyrko <mkostyrko@gmail.com>
2021-08-10 18:04:47 +0200

    initial commit

---
+++ github-issues/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HelloWorld from './components/HelloWorld.js'
+// not support yet for m1
+// import './scss/main.scss';
+
+function App() {
+  return (
+    <>
+      <HelloWorld />
+    </>
+  );
+}
+
+export default App;
---
+++ github-issues/src/components/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const HelloWorld = () => {
+    return (
+        <div class='greeting'>
+            Hello world!
+        </div>
+    );
+}
+
+export default HelloWorld;
